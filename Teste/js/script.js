ff7a9a9bd7feee5586ec5bf3b6ddd405dd8b2213
Altair Preto de Godoi Junior <altair.godoi@fatec.sp.gov.br>
2025-04-07 13:59:50 -0300

    UpdateFiles

--- Teste/js/script.js
+++ Teste/js/script.js
@@ -1,4 +1,6 @@
-/* function cliquei(){
+/*primeiro teste
+
+ function cliquei(){
     alert("OLA, "+ nome)
 }*/
 
@@ -7,11 +9,12 @@ var form = document.getElementById("formulario")
 form.addEventListener("submit", function(sub) {
     sub.preventDefault(); // impedindo envio padrão do forms
 
-    // pegando os valores do input
-    const name = document.getElementById("name").value;
+    // pegando os valores do input e declarando as variaveis
+    const name = document.getElementById("name").value
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
+    //Windows alert com os dados do usuário
     alert(
         `Nome: ${name} 
         Email: ${email} 
